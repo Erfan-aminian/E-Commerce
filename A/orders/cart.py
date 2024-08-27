ea8df4115dfa14a57f976630c590c12b85adbaa2
Erfan-aminian <aminian158@gmail.com>
2024-08-27 18:36:11 +0430

    add cart add,save and complete cart view

--- A/orders/cart.py
+++ A/orders/cart.py
@@ -1,11 +1,22 @@
 
 
 CART_SESsION_ID = 'cart'
-class cart:
+class Cart:
     def __init__(self, request):
         self.session = request.session
         cart = self.session.get(CART_SESsION_ID)
         if not cart:
             cart = self.session[CART_SESsION_ID] = {}
         self.cart =cart
+
+    def add(self,product,quantity):
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity':0, 'price':str(product.price)}
+        self.cart[product_id][quantity] += quantity
+        self.save()
+
+    def save(self):
+        self.session.modified = True
+
 
--- A/orders/views.py
+++ A/orders/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-
+from forms import CartAddform
+from home.models import Product
+from cart import Cart
 # Create your views here.
 class CartView(View):
     def get(self, request):
@@ -8,4 +10,10 @@ class CartView(View):
 
 class CartAddView(View):
     def post(self, request, product_id):
-        pass
+        cart = Cart(request)
+        product = get_object_or_404(Product, id=product_id)
+        form = CartAddform(request.POST)
+        if form.is_valid():
+            cart.add(product, form.cleaned_data['quantity'])
+        return redirect('orders:cart')
+
