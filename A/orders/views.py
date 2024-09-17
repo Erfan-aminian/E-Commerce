32e3a6159442f5a561deac55cee6f1fb6b36077c
Erfan-aminian <aminian158@gmail.com>
2024-09-17 20:48:18 +0430

    add zarin pal to pay orders

--- A/orders/views.py
+++ A/orders/views.py
@@ -4,7 +4,11 @@ from .forms import CartAddform
 from home.models import Product
 from .cart import Cart
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order,OrderItem
+from .models import Order, OrderItem
+import requests
+import json
+from A import settings
+from django.http import HttpResponse
 # Create your views here.
 class CartView(View):
     def get(self, request):
@@ -41,3 +45,59 @@ class OrderCreateView(LoginRequiredMixin, View):
             OrderItem.objects.create(order = order, product=item['product'], price=item['price'], quantity=item['quantity'])
         cart.clear()
         return redirect('orders:order_detail', order.id)
+#? sandbox merchant
+if settings.SANDBOX:
+    sandbox = 'sandbox'
+else:
+    sandbox = 'www'
+
+ZP_API_REQUEST = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentRequest.json"
+ZP_API_VERIFY = f"https://{sandbox}.zarinpal.com/pg/rest/WebGate/PaymentVerification.json"
+ZP_API_STARTPAY = f"https://{sandbox}.zarinpal.com/pg/StartPay/"
+description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
+CallbackURL = 'http://127.0.0.1:8080/orders/verify/'
+
+class OrderPayView(LoginRequiredMixin, View):
+    def get(self, request, order_id):
+        order = Order.objects.get(id=order_id)
+        data = {
+            "MerchantID": settings.MERCHANT,
+            "Amount": order.get_total_price(),
+            "Description": description,
+            "Phone": request.user.phone_number,
+            "CallbackURL": CallbackURL,
+        }
+        data = json.dumps(data)
+        headers = {'content-type': 'application/json', 'content-length': str(len(data))}
+        try:
+            response = requests.post(ZP_API_REQUEST, data=data, headers=headers, timeout=10)
+
+            if response.status_code == 200:
+                response = response.json()
+                if response['Status'] == 100:
+                    return {'status': True, 'url': ZP_API_STARTPAY + str(response['Authority']),
+                            'authority': response['Authority']}
+                else:
+                    return {'status': False, 'code': str(response['Status'])}
+            return response
+
+        except requests.exceptions.Timeout:
+            return {'status': False, 'code': 'timeout'}
+        except requests.exceptions.ConnectionError:
+            return {'status': False, 'code': 'connection error'}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
