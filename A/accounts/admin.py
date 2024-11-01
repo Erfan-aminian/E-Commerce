7ffff4ed9bfe372d82db658aa3878abb1af6e70f
Erfan-aminian <aminian158@gmail.com>
2024-11-01 15:54:54 +0330

    add user permissions

--- A/accounts/admin.py
+++ A/accounts/admin.py
@@ -17,15 +17,14 @@ class UserAdmin(BaseUserAdmin):
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('email', 'phone_number', 'full_name', 'password')}),
-        ('permission',{'fields': ('is_active', 'is_admin', 'last_login')}),
+        ('permission',{'fields': ('is_active', 'is_admin','is_superuser', 'last_login', 'groups' , 'user_permissions')}),
     )
     add_fieldsets = (
         (None, {'fields': ('phone_number', 'email', 'full_name', 'password1', 'password2')}),
     )
     search_fields = ('is_admin',)
     ordering = ('full_name',)
-    filter_horizontal = ()
+    filter_horizontal = ('groups' , 'user_permissions')
 
 
-admin.site.unregister(Group)
 admin.site.register(User, UserAdmin)
